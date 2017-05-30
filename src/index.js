2e008d098eaebc1e35842abfb4200826f7d2be61
Vinicius Vinhas <vinhas@cdts.fiocruz.br>
2017-05-30 16:34:28 -0300

    Updating Store usage, creating new routes

--- src/index.js
+++ src/index.js
@@ -10,31 +10,53 @@ const generateFakeUser = () => ({
   lastName: faker.name.lastName()
 })
 
+const createUser = (email, password, firstName, lastName) => Map({
+  _id: uuid.v4(),
+  email,
+  password,
+  firstName,
+  lastName
+})
+const sameId = (user, id) => user.get('_id') === id
+
 const init = (fake) => {
   let data = []
 
   while (data.length < fake) {
     data.push(generateFakeUser())
   }
-
+  
   return { data }
 }
 
 const make = (router, store) => {
   // Get all users
   router.get('/all', (req, res) => {
-    res.json(store.getState().get('data').toJSON())
+    const users = store.output().get('data')
+    res.json(users ? users.toJSON() : [])
+  })
+  // Get a single user
+  router.get('/:id', (req, res) => {
+    const user = store.output()
+      .get('data')  
+      .find(user => sameId(user, req.params.id))
+    res.json(user ? user.toJSON() : {})
   })
   // Save a user
   router.post('/', (req, res) => {
-    const { email, name } = req.body
+    const { email, password, firstName, lastName } = req.body
     const newState = store.getState()
-      .update('data', users => users.push(Map({
-        _id: uuid.v4(),
-        email,
-        name
-      })))
-    store.updateState(newState)
+      .update('data', users => users.push(createUser(email, password, firstName, lastName)))
+    store.setState(newState)
+    res.status(200).end()
+  })
+  // Delete a user
+  router.delete('/delete/:id', (req, res) => {
+    const newState = store.getState()
+      .update('data', users => users.filter(user => {
+        return !sameId(user, req.params.id)
+      }))
+    store.setState(newState)
     res.status(200).end()
   })
 
